fb89cb7dc34d83cfc4124fa8b66a1ee85c5cf135
DESKTOP-QU92HU2\mafra <matheus.franco@65a.com.br>
2020-08-24 18:40:37 -0300

    environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCXAFGlBOgfrMNzcnued6Eh8dzKMTUxFmM",
-    authDomain: "super-heroes-e224c.firebaseapp.com",
-    databaseURL: "https://super-heroes-e224c.firebaseio.com",
-    projectId: "super-heroes-e224c",
-    storageBucket: "super-heroes-e224c.appspot.com",
-    messagingSenderId: "1027194514129",
-    appId: "1:1027194514129:web:d554b54e6f217a5228f3f0",
-    measurementId: "G-SJLFCM56TM"
+    apiKey: 'AIzaSyCXAFGlBOgfrMNzcnued6Eh8dzKMTUxFmM',
+    authDomain: 'super-heroes-e224c.firebaseapp.com',
+    databaseURL: 'https://super-heroes-e224c.firebaseio.com',
+    projectId: 'super-heroes-e224c',
+    storageBucket: 'super-heroes-e224c.appspot.com',
+    messagingSenderId: '1027194514129',
+    appId: '1:1027194514129:web:d554b54e6f217a5228f3f0',
+    measurementId: 'G-SJLFCM56TM'
   }
 };
